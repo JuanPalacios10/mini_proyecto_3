65af06c90af2eda0dc0a590fc0cd71110bf782fc
Rojasjuan08 <144349943+Rojasjuan08@users.noreply.github.com>
2023-11-30 18:47:29 -0500

    Complemente implementación lógica gui

--- src/model/Ciudades.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public enum Ciudades {
-    Alcala, Andalucia, Ansermanuevo, Argelia, Bolivar, Buenaventura, Buga, Bugalagrande, Caicedonia, Cali, Candelaria,
-    Cartago, Dagua, El_Aguila, El_Cairo, El_Cerrito, El_Dovio, Florida, Ginebra, Guacari, Jamundi, La_Cumbre, La_Union,
-    La_Victoria, Obando, Palmira, Pradera, Restrepo, Riofrio, Roldanillo, San_Pedro, Sevilla, Toro, Trujillo, Tulua,
-    Ulloa, Versalles, Vijes, Yotoco, Yumbo, Zarzal, Darien;
-}
